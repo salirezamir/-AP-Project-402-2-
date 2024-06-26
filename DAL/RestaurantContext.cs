5cf339497fe873c04e72217f816aa5609ba7ca16
Alireza Mirabedini <mirabedini.alireza@gmail.com>
2024-06-26 15:06:14 +0330

    Db test Ui test

---
+++ DAL/RestaurantContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant_Manager.Models;
+using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Manager.DAL
+{
+    internal class RestaurantContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Server"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
